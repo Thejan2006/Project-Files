e3ecb9e76bcc3120541e5d75437fea8ad72aac14
Thejan Bandara <hmagthejanbandar@gmail.com>
2026-02-06 22:33:51 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,124 @@
+
+// let x
+// console.log(x,typeof x);
+// x = 6
+// console.log(x,typeof x);
+// x = +6 + "6"
+//  console.log(x, typeof x);
+// x = !x
+
+// PARSEINT
+
+// convert to number format
+
+// let y = "thejan 1234"
+// console.log(y, typeof y);
+
+// let y = parseInt(" 1234 thejan ")
+// console.log(y , typeof y);
+
+//  ARITHMETIC OPERATORS
+
+// let num1 = 4
+// let num2 = 8
+// let result = num1/num2
+// console.log(result)
+
+// let num3 = true
+// let num4= false
+// let result2 = num3 + num4
+// console.log(result2)
+
+// let num = 4
+// num + 3
+// console.log(num)
+
+// let num6 = 4
+// num += 3
+// console.log(num6)
+
+// num = num -1
+// ++num pre increment
+// num++ post increment
+
+// let num = 4
+// let x = num++  // num++ post increment
+// console.log(x,num)
+
+
+// let num = 4
+// let y= ++num // ++num pre increment
+// console.log(y,num)
+
+// neet to do num = num +1 can do in
+// num ++
+
+// let num7 = 4
+// num ++ 3  this is worng can get output
+// console.log(num7)
+
+    
+
+
+
+// let num = 5
+// let result = num * num * num
+// console.log(result)
+
+// can do  like this 
+
+// let num = 5
+// let result = 5**3
+// console.log(result)
+
+// /
+
+// let num = 5
+// let result = Math.pow(5,3)
+// console.log(result)
+
+
+
+// /  RELATIONAL OPERATORS
+   
+// let dtat = 4 >= 5
+// console.log(dtat)
+
+// let x = "pen"
+// let y= "pencil" 
+// let data = x > y
+// console.log(data)
+
+
+
+// let k = 2
+// let t = 2
+// let datatic = k === t
+// console.log(datatic)
+
+
+
+// /// logical operators
+
+// let x = 5, y = 6,  z = 7
+// let data =  x < y && x < z
+
+// console.log(data)
+
+
+// let x = 5, y = 6,  z = 7
+// let data =  x > y || x > z
+
+
+// console.log(data)
+
+// let x = 5, y = 6,  z = 7
+// let data =  x > y || x > z
+// let n = !data
+
+// console.log(data,n)
+
+
+
+
+
